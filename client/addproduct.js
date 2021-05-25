32f72c66844862dc768603e28da4ce89beeaf0f1
Manohar Choudary <manoharchoudaryv@gmail.com>
2021-05-25 17:44:25 +0530

    status change

--- client/addproduct.js
+++ client/addproduct.js
@@ -1,6 +1,6 @@
 var allProducts = [];
 
-window.onload = function(){
+window.onload = function () {
     getAllProducts();
 }
 
@@ -34,7 +34,7 @@ async function addNewProduct(data) {
 }
 
 
-async function getAllProducts(){
+async function getAllProducts() {
     let uri = `http://localhost:3000/api/products/getAllProducts`;
     let response = await fetch(uri);
     let data = await response.json();
@@ -43,24 +43,61 @@ async function getAllProducts(){
     console.log(data);
 }
 
-function showAllProducts(){
+function showAllProducts() {
     let table = document.getElementById("pTable");
-    while(table.rows.length>1){
+    while (table.rows.length > 1) {
         table.deleteRow(1);
     }
-    for(let i=0; i<allProducts.length;i++){
+    for (let i = 0; i < allProducts.length; i++) {
         let obj = allProducts[i];
+        let cb = obj.isActive?"checked":'';
         let tRow = `<tr>
-                <td>${i+1}</td>
+                <td>${i + 1}</td>
                 <td>${obj.name}</td>
                 <td>$ ${obj.mrp}</td>
                 <td>${obj.ram}</td>
                 <td>${obj.storage}</td>
                 <td>${obj.color}</td>
                 <td>${obj.discount} %</td>
-                <td></td>
-                <td></td>
+                <td>
+                    <button class="btn btn-info" onclick="onEdit('${obj._id}')">Edit</button>
+                    <button class="btn btn-danger" onclick="onDelete('${obj._id}')">Delete</button>
+                </td>
+                <td>
+                    <input type="checkbox" ${cb} onchange="onStatus('${obj._id}',event)">
+                </td>
         </tr>`;
-        table.insertAdjacentHTML("beforeend",tRow);
+        table.insertAdjacentHTML("beforeend", tRow);
     }
+}
+
+async function onEdit(id){
+    console.log(id);
+    let uri = `http://localhost:3000/api/products/getProductById/${id}`;
+    let response = await fetch(uri);
+    let data = await response.json();
+    console.log(data);
+}
+
+function onDelete(id){
+    console.log(id);
+}
+ 
+async function onStatus(id,e){
+
+    let data = {
+        isActive:e.target.checked,
+        id:id
+    }
+    let uri = `http://localhost:3000/api/products/updateProduct`;
+    let options = {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    }
+    let response = await fetch(uri, options);
+    getAllProducts();
 }
